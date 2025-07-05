f17c7d84a85e128d358a8a92cb1217783d536534
Joepriestley <joeamonoo1114@gmail.com>
2025-07-05 17:18:31 +0100

    corrected mysql.connector.Error to handle exceptions in mysqlserver.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -13,7 +13,7 @@ def myConnection():
         )
         print("Connection successful!")
         return conn
-    except Error as e:
+    except mysql.connector.Error as e:
         
         print(f"Database connection error: {e}")
         return None
@@ -23,6 +23,9 @@ def create_database():
     try:
         
         connn = myConnection()
+        if connn is None:
+            print("Could not connect to database")
+            return 
         cursor =connn.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
@@ -30,7 +33,7 @@ def create_database():
         connn.close()
         print("Database 'alx_book_store' created successfully!")
         
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error creating database:{e}")
 		 
 create_database()
